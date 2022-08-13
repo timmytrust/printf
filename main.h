b7a810a28f06ec3ae359ec1d4dfa2c7f1d79c285
timmytrust <timmytrust5@gmail.com>
2022-08-13 13:43:24 -0700

    singly linked

---
+++ main.h
@@ -0,0 +1,29 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <stdarg.h>
+
+#include <limits.h>
+
+#include <unistd.h>
+
+
+
+/**
+ * struct format - match the conversion specifiers for printf
+ */
+
+
+
+int _printf(const char *format, ...);
+
+
+
+#endif
